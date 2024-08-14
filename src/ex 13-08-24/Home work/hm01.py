6340c00ee332721033e492a00939fb992d2df5c0
khalid <saifullakattubadi@gmail.com>
2024-08-14 20:25:28 +0100

    10,13 and home work

--- src/ex 13-08-24/Home work/hm01.py
+++ src/ex 13-08-24/Home work/hm01.py
@@ -1,5 +1,4 @@
 '''create progamme of table using user input and formatting concepts.
-'''
 
 a = int(input("num"))
 print(f"{a}*1= {a*1}")
@@ -12,3 +11,28 @@ print(f"{a}*7= {a*7}")
 print(f"{a}*8= {a*8}")
 print(f"{a}*9= {a*9}")
 print(f"{a}*10= {a*10}")
+'''
+Task #1
+
+
+# Homework
+# Can you create a Program I will give you number(userinput and print table)
+# f"{}" String format concept
+# User input - num int -> 10, 100, -1, 2, 3.14 -> input
+# 9x1 = 9
+# 9x2 = 18... till 10
+
+
+
+
+a = int(input("num"))
+print((f"{a}*1 ={a*1}"))
+print((f"{a}*2 ={a*2}"))
+print((f"{a}*3 ={a*3}"))
+print((f"{a}*4 ={a*4}"))
+print((f"{a}*5 ={a*5}"))
+print((f"{a}*6 ={a*6}"))
+print((f"{a}*7 ={a*7}"))
+print((f"{a}*8 ={a*8}"))
+print((f"{a}*9 ={a*9}"))
+print((f"{a}*10={a*10}"))
---
+++ src/ex 13-08-24/Home work/hm02.py
@@ -0,0 +1,17 @@
+#Task #2
+
+#Create a program , take 2 inputs from the user num1, num2 and give them
+#max
+#pow num1 to num2
+#sub, mul, sum, div.
+#Format your out with f{""}
+
+b = int(input("num1"))
+c = int(input("num2"))
+print("power of num1&num2", f"{pow(b,c)}")
+print("Max of num1&num2", f"{max(b,c)}")
+print("Min of num1&num2", f"{min(b,c)}")
+print("sum of num1&num2", f"{(b+c)}")
+print("subtract of num1&num2", f"{(b-c)}")
+print("Multiply of num1&num2", f"{(b*c)}")
+print("Division of num1&num2", f"{(b/c)}")
