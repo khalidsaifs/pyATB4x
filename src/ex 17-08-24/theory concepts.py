33466f684f8e6e06521d1f040b618d1702c8bee0
khalid <saifullakattubadi@gmail.com>
2024-08-28 18:56:11 +0100

    Task7,8,9

---
+++ src/ex 17-08-24/theory concepts.py
@@ -0,0 +1,35 @@
+#Literals
+'''litterals are the assigned values,
+litterals can be non char or chrecter one.
+variable = vaiable value(literal)
+variable = identifiers
+vaiable value = (literal)
+
+typers of Literals ar as follows:
+
+int = 123
+float = 4.56
+boolean = true or false
+complex = 4+i5
+list = [""]
+tuple = ("")
+
+charecter Literals are
+
+\n = new line
+\t = tab space
+\b = back space
+r = raw string{ r is used in automation API and web page, while accessing file type we use r concept }
+'''
+'''
+#examples for charecter literals as follows
+
+print("mohammed \n khalid")
+print("mohammed \t khalid")
+print("mohammed \b khalid")
+age = r"any place\n.txt"
+print(age)
+'''
+#Escape sequence:
+
+print("mohammed'khalid")
